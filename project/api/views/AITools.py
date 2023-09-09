33b9bab7450e9de512a596c538d36dde4705c1e9
Rafiur Rahman <bsrsoftbd@gmail.com>
2023-09-09 02:30:57 +0100

    Update AITools.py

--- project/api/views/AITools.py
+++ project/api/views/AITools.py
@@ -192,9 +192,7 @@ class GenerateAIText(APIView):
         serializer = AITextSerializer(data=request.data)
         if serializer.is_valid():
             text = serializer.validated_data['text']
-            api_key = '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'
-            openai.api_key =  api_key
-            # os.getenv("OPENAI_API_KEY")
+            openai.api_key =  os.getenv("OPENAI_API_KEY")
             chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": text}])
             expert_reply = chat_completion['choices'][0]['message']['content']
             # expert_reply = 'Dummy text'

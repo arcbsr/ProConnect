6240ce16f8422b165e4847ed49e6bd9185d5b8e4
Rafiur Rahman <bsrsoftbd@gmail.com>
2023-09-17 22:00:26 +0100

    add country and area user profile

--- project/api/models/UserProfile.py
+++ project/api/models/UserProfile.py
@@ -25,7 +25,8 @@ class UserProfile(models.Model):
     profile_image = VersatileImageField('Image',upload_to='profile_images/',blank=True, null=True)
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
-
+    country = models.CharField(max_length=100,null=True, blank=True)
+    area = models.CharField(max_length=100,null=True, blank=True)
 def __str__(self):
         return self.name
 
